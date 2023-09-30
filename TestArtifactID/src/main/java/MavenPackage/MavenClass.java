6ec4a6d388eba07893526e290f1923112928cd02
vvajjarx <vvajjarx@vvajjarx-mobl.gar.corp.intel.com>
2023-09-30 12:19:48 +0530

    Signed-off-by: vvajjarx <vvajjarx@vvajjarx-mobl.gar.corp.intel.com>

---
+++ TestArtifactID/src/main/java/MavenPackage/MavenClass.java
@@ -0,0 +1,10 @@
+package MavenPackage;
+
+public class MavenClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Maven");
+
+	}
+
+}
---
+++ TestArtifactID/src/main/java/MavenPackage/TestNG.java
@@ -0,0 +1,12 @@
+package MavenPackage;
+
+import org.testng.annotations.Test;
+
+public class TestNG {
+	
+	@Test
+	public void a2() {
+		System.out.println("Hello a2 method");	
+	}
+
+}
