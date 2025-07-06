40583909b66b1e5ad5f48f082795ceee662e8175
vinod <vinod@Vevi>
2025-07-06 13:36:34 +0530

    Adding new project

---
+++ Test_Git_Project/src/MyPackage/MyJava.java
@@ -0,0 +1,10 @@
+package MyPackage;
+
+public class MyJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("hello world");
+	}
+
+}
---
+++ Test_Git_Project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Test_Git_Project {
+}
