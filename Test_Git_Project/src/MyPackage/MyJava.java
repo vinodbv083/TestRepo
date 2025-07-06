a2567ddbcc1b7a8ee720fd669ae341d7d6f8d658
vinodbv083 <vinodbv083@gmail.com>
2025-07-06 14:32:02 +0530

    new changes are done

--- Test_Git_Project/src/MyPackage/MyJava.java
+++ Test_Git_Project/src/MyPackage/MyJava.java
@@ -5,7 +5,6 @@ public class MyJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("hello world");
-      System.out.println("hello Veena");
 	}
 
 }
