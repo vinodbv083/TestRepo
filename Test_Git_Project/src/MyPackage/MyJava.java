e09f3d2f800b628e530e1ff4e6a3ceb46fc3b456
vinod <vinod@Vevi>
2025-07-06 14:15:32 +0530

    corrected

--- Test_Git_Project/src/MyPackage/MyJava.java
+++ Test_Git_Project/src/MyPackage/MyJava.java
@@ -4,7 +4,7 @@ public class MyJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("hello wooooorrld");
+      System.out.println("My hello world");
 	}
 
 }
